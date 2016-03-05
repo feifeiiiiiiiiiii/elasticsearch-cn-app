62920489a6179c51d16526cca927d3c72e347406
qinpengfei <qinpengfei.2008@163.com>
2016-03-05 22:58:02 +0800

    * Add: elasticsearch中文社区代理api

---
+++ proxy/server.js
@@ -0,0 +1,69 @@
+var restify = require('restify');
+var request = require('request');
+var util = require('./util');
+
+var server = restify.createServer({
+  name: 'escnproxy',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/topics', function (req, res, next) {
+  var page = 1;
+  if (req.params.page) page = req.params.page;
+  var base = 'http://proxy.elasticsearch.thnuclub.com/topic/page-'+page;
+  request(base, function(err, response, body) {
+  	if(err) {
+  		return next(err);
+  	}
+  	try {
+  		data = util.parseTopic(body);
+  		res.send(200, data);
+  		return next();
+  	} catch(e) {
+  		return next("parse error");
+  	}
+  })
+});
+
+server.get('/users', function (req, res, next) {
+  var page = 1;
+  if (req.params.page) page = req.params.page;
+  var base = 'http://proxy.elasticsearch.thnuclub.com/people/page-'+page;
+  request(base, function(err, response, body) {
+  	if(err) {
+  		return next(err);
+  	}
+  	try {
+  		data = util.parseUser(body);
+  		res.send(200, data);
+  		return next();
+  	} catch(e) {
+  		return next("parse error");
+  	}
+  })
+});
+
+server.get('/questions', function (req, res, next) {
+  var page = 1;
+  if (req.params.page) page = req.params.page;
+  var base = 'http://proxy.elasticsearch.thnuclub.com/sort_type-new__day-0__is_recommend-0__page-'+page;
+  request(base, function(err, response, body) {
+  	if(err) {
+  		return next(err);
+  	}
+  	try {
+  		data = util.parseQuestion(body);
+  		res.send(200, data);
+  		return next();
+  	} catch(e) {
+  		return next("parse error");
+  	}
+  })
+});
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
---
+++ proxy/util.js
@@ -0,0 +1,96 @@
+var cheerio = require('cheerio');
+var request = require('request');
+
+parseUser = function(body) {
+	var $ = cheerio.load(body);
+	var avtars = [];
+	$('.aw-user-img>img').each(function(i) {
+		avtars.push($(this)[0].attribs.src);
+	});
+	var nicknames = [];
+	$('.aw-item>p.title').each(function(i) {
+		nicknames.push($(this).text().trim());
+	})
+	var descs = [];
+	$('.aw-item>p.signature').each(function() {
+		descs.push($(this).text().trim());
+	});
+	var meta = {
+		prestige: [],
+		score: [],
+		agree: [],
+		thank: [],
+	}
+	$('.aw-item>div.meta').each(function(i, e) {
+		var tmp = ['prestige', 'score', 'agree', 'thank'];
+		var i = 0;
+		$(e).find('span>b').each(function() {
+			meta[tmp[i]].push($(this).text());
+			i++;
+		})
+	})
+	var tags = [];
+	$('.aw-item .topic-bar').each(function(i, e) {
+		var tmp = "";
+		$(e).find(".topic-tag").each(function() {
+			if(tmp.length) tmp += " ";
+			tmp += $(this).text().replace(/\s+/g, '');
+		})
+		tags.push(tmp);
+	})
+	var data = [];
+	for(var i = 0; i < avtars.length; ++i) {
+		data.push({
+			img: avtars[i],
+			nickname: nicknames[i],
+			desc: descs[i],
+			topic: tags[i],
+			prestige: meta['prestige'][i] || 0,
+			score: meta['score'][i] || 0,
+			agree: meta['agree'][i] || 0,
+			thank: meta['thank'][i] || 0
+		})
+	}
+	return data;
+}
+
+parseTopic = function(body) {
+		var $ = cheerio.load(body);
+		var data = [];
+
+		$('.aw-item').each(function(i, e) {
+			var tmp = {};
+			tmp['img'] = $(e).find('a.img')[0].attribs.href;
+			tmp['id'] = $(e).find('a.img')[0].attribs['data-id'];
+			tmp['tag'] = $(e).find('.topic-tag').text().replace(/\s+/g, '');
+			var dict = ['tag', 'talk', 'desc'];
+			var i = 0;
+			$(e).find('p').each(function() {
+				tmp[dict[i]] = $(this).text().replace(/\s+/g, '');
+				i++;
+			})
+			data.push(tmp);
+		})
+		return data;
+}
+
+parseQuestion = function(body) {
+	var $ = cheerio.load(body);
+	var data = [];
+	$('.aw-item').each(function(i, e) {
+		var tmp = {};
+		tmp['img'] = $(e).find('.aw-user-name img')[0].attribs.src;
+		tmp['question'] = $(e).find('.aw-question-content h4').text().replace(/\s+/g, '');
+		var arr = $(e).find('.aw-question-content h4>a')[0].attribs.href.split('/');
+		tmp['id'] = arr[arr.length-1];
+		tmp['desc'] = $(e).find('.aw-question-content p').text().replace(/\s+/g, '');
+		data.push(tmp);
+	})
+	return data;
+}
+
+module.exports = {
+	parseTopic: parseTopic,
+	parseQuestion: parseQuestion,
+	parseUser: parseUser
+}
